4f51cfbd145e99fd601246c6495d58e72973500a
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2022-08-03 18:11:42 +0530

    added success and failure page

--- yt.js
+++ yt.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -20,6 +21,16 @@ app.post("/", function(req,res){
         .pipe(fs.createWriteStream('video.mp4'));
 
     console.log("the video is download in your project file directory with a name video.mp4 ");
+
+    const request = https.request(URL,function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+    
+    });
+    request.end();
 });
 
 app.listen(3000, function(){
